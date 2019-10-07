b1100bd583ae1681417311c968f37df8fd873a93
Keno Moenck <keno.moenck@tu-harburg.de>
2019-10-07 21:29:37 +0200

    running

--- FFT/main.asm
+++ FFT/main.asm
@@ -28,6 +28,9 @@ RESET:
 .org SPIaddr ; SPI Serial Transfer Complete interrupt address
 	jmp DISPLAY_ROUTINE
 
+.org ADCCaddr
+	jmp ADC_CONVERSION_COMPLETE
+
 .org INT_VECTORS_SIZE ; end address of 
 
 
@@ -44,6 +47,8 @@ RESET:
 
 .include "FFT.inc"
 .include "DisplayRoutine.inc"
+.include "ADC.inc"
+.include "USART.inc"
 
 
 ;------------------------------------------------------
@@ -77,7 +82,10 @@ INIT:
 		sFFT_DISPLAY_POINTER_L: .byte 1
 		sFFT_DISPLAY_POINTER_H: .byte 1
 
-		.org 0x0240
+		sADC_SAMPLES_POINTER_L: .byte 1
+		sADC_SAMPLES_POINTER_H: .byte 1
+
+		.org 0x0400
 		sFFT_POINTS: .byte FFT_POINT_SIZE
 		
 		.org 0x02d0
@@ -121,19 +129,18 @@ INIT:
 	
 
 	;------------------------------------------------------
-	; interrupt
+	; timer and interrupt for display routine
 	;------------------------------------------------------
-	
 	; set 8bit timer/counter 0
-	ldi rTEMPA, (1<<CS01)|(1<<CS00) ; set clock select bit (see p. 107), clk / 64
+	ldi rTEMPA, (1<<CS02)|(1<<CS00) ; set clock select bit (see p. 107), clk / 1024
 	out TCCR0B, rTEMPA ; Timer/Counter 0 Control Register B
-	ldi rTEMPA, 80 
+	ldi rTEMPA, 40
 	out OCR0A, rTEMPA ; Output Compare Register A
 
 	; set interrupt
 	ldi rTEMPA, (1<<OCIE0A) ; OCIE0A: Timer/Counter0 Output Compare Match A Interrupt Enable
 	sts TIMSK0, rTEMPA ; Timer/Counter 0 Interrupt Mask Register
-
+	
 
 	;------------------------------------------------------
 	; spi
@@ -155,19 +162,78 @@ INIT:
 	
 	
 	;------------------------------------------------------
+	; timer and interrupt for adc
+	;------------------------------------------------------
+	
+	ldi rTEMPCL, 200
+	ldi rTEMPCH, 0
+	sts OCR1AH, rTEMPCH ; Output Compare Register A
+	sts OCR1AL, rTEMPCL ; Output Compare Register A
+
+	ldi rTEMPCL, 190
+	sts OCR1BH, rTEMPCH ; Output Compare Register B
+	sts OCR1BL, rTEMPCL ; Output Compare Register B
+	
+	; set 16bit timer/counter 1
+	ldi rTEMPA, (1<<CS11)|(1<<CTC1) 
+	; CS11: set clock select bit (see p. 134), clk / 8
+	; CTC1: CTC mode
+	sts TCCR1B, rTEMPA ; Timer/Counter 1 Control Register B
+	
+
+	;------------------------------------------------------
+	; adc
+	;------------------------------------------------------
+	; see p. 255
+	ldi rTEMPA, (1<<REFS0)|(1<<ADLAR) ; ADMUX: ADC Multiplexer Selection Register
+	; REFS0: AVCC with external capacitor at AREF pin
+	; ADLAR: ADC Left Adjust Result
+	; 0: Single Ended Input: ADC0
+	sts ADMUX, rTEMPA
+	
+	; see p. 258
+	ldi rTEMPA, (1<<ADTS2)|(1<<ADTS0)
+	; Timer/Counter1 Compare Match B
+	sts ADCSRB, rTEMPA
+
+	ldi rTEMPA, (1<<ADEN)|(1<<ADATE)|(1<<ADIE)|(1<<ADSC)|(1<<ADPS1)|(1<<ADPS2)
+	; ADEN: ADC Enable
+	; ADATE: ADC Auto Trigger Enable
+	; ADIE: ADC Interrupt Enable
+	; ADSC: ADC Start Conversion
+	; ADPS0, ADPS1: ADC prescaler selection -> 64
+	sts ADCSRA, rTEMPA
+	
+
+	;------------------------------------------------------
+	; adc sample point counter
+	;------------------------------------------------------
+
+	ldi ZL, low(sFFT_POINTS)
+	ldi ZH, high(sFFT_POINTS)
+
+	sts sADC_SAMPLES_POINTER_L, ZL
+	sts sADC_SAMPLES_POINTER_H, ZH
+
+
+	;------------------------------------------------------
 	; misc
 	;------------------------------------------------------
 	
 	; for testing
 	;rcall DISPLAY_ROUTINE_FILL_DEBUG
-	rcall FFT_FILL_SRAM
+	;rcall FFT_FILL_SRAM
 
-	; debug: set pin PC6 and PC7 as output
-	ldi rTEMPA, (1<<6)|(1<<7)
+	; debug: set pin PC5, PC6 and PC7 as output
+	ldi rTEMPA, (1<<5)|(1<<6)|(1<<7)
 	out DDRC, rTEMPA
 	ldi rTEMPA, 0x00
 	out PortC, rTEMPA
 	
+	; usart init
+	rcall USART_INIT
+
+
 	sei ; global interrupt enable
 
 
@@ -177,8 +243,11 @@ INIT:
 	
 Program:
 	
+	; check if adc interrupt flag is set
+	; if not -> FFT
+	SKBS ADCSRA, ADIE, rTEMPA
 	rcall FFT
-
+	
 	rjmp Program
 
 ;------------------------------------------------------
